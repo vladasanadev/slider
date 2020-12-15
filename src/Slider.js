22de49d5a4eabc6990540feecf0875c7daf55eb8
Vlada Kandyba <hello.vladasana@gmail.com>
2020-12-15 02:28:03 +0200

    fixed slider thumb image

--- src/Slider.js
+++ src/Slider.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import image from "./klipartz9.png";
 
 const sliderThumbStyles = (props) => `
   width: 25px;
   height: 25px;
-  background: ${props.color};
+  // background: ${props.color};
   cursor: pointer;
 transform: scale(${props.scale}) rotate(-90deg);
   
@@ -14,10 +15,7 @@ const Styles = styled.div`
   display: flex;
   align-items: center;
   color: #888;
-  position: absolute;
-  left: 25%;
-  top: 45%;
-  width: 100%;
+
   input {
     -webkit-transform: rotate(90deg);
     -moz-transform: rotate(90deg);
@@ -30,9 +28,13 @@ const Styles = styled.div`
     } */
   }
   .slider {
+    position: absolute;
+    left: 25%;
+    top: 45%;
+
     -webkit-appearance: none;
     width: 100%;
-    height: 3px;
+    height: 6px;
     border-radius: 5px;
     background: #efefef;
     outline: none;
@@ -40,7 +42,9 @@ const Styles = styled.div`
     &::-webkit-slider-thumb {
       -webkit-appearance: none;
       appearance: none;
-      background-image: url("https://img.icons8.com/officel/16/000000/hearts.png");
+      background-image: url(${image});
+      filter: drop-shadow(2px 5px 4px rgba(0, 0, 0, 0.3));
+
       background-size: contain;
       background-position: center center;
       background-repeat: no-repeat;
